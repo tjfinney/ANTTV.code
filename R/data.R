88e1d36e22d5d16a1f50e3f4efa1bd5c07d9b11f
Tim Finney <tjf@tfinney.net>
2022-03-13 21:06:21 +0800

    Add documentation for Mark.UBS4.

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Gospel of Mark textual variation data extracted from UBS4 (*Greek New Testament*, 4th rev. ed., Stuttgart: United Bible Societies, 1993)
+#'
+#' A data frame that specifies the readings of witnesses for variation sites found in the UBS4 apparatus. Numerals encode readings according to the order given in the apparatus: i.e. 1 = first reading, 2 = second reading, ...
+#'
+#' @format A data frame with 267 rows and 142 columns:
+#' \describe{
+#'   \item{rows}{witnesses}
+#'   \item{columns}{variation sites}
+#'   ...
+#' }
+#' @source \url{https://zenodo.org/record/4064629}
+"Mark.UBS4"
