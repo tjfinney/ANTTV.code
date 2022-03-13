9ee8d7100ecf6a4b7ea25d9363c3d251b64af607
Tim Finney <tjf@tfinney.net>
2022-03-13 21:22:42 +0800

    Tweak doc for Mark.UBS4

--- R/data.R
+++ R/data.R
@@ -1,11 +1,10 @@
-#' Gospel of Mark textual variation data extracted from UBS4 (*Greek New Testament*, 4th rev. ed., Stuttgart: United Bible Societies, 1993)
+#' Gospel of Mark textual variation data extracted from UBS4 (*Greek New Testament*, 4th rev. ed., Stuttgart: United Bible Societies, 1993).
 #'
-#' A data frame that specifies the readings of witnesses for variation sites found in the UBS4 apparatus. Numerals encode readings according to the order given in the apparatus: i.e. 1 = first reading, 2 = second reading, ...
+#' A data frame that specifies the readings of witnesses for variation sites found in the UBS4 apparatus. Numerals encode readings according to the order given in the apparatus: i.e. 1 = first reading, 2 = second reading, etc. There is one row per witness and one column per variation site contained in the apparatus. This data frame was constructed by Richard Mallett.
 #'
 #' @format A data frame with 267 rows and 142 columns:
 #' \describe{
-#'   \item{rows}{witnesses}
-#'   \item{columns}{variation sites}
+#'   \item{Mk.1.1}{Codes representing readings found in the apparatus entry at Mark 1.1.}
 #'   ...
 #' }
 #' @source \url{https://zenodo.org/record/4064629}
