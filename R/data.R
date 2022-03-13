b12620103855062d5c209801bd1e43a6f00fd812
Tim Finney <tjf@tfinney.net>
2022-03-13 21:25:08 +0800

    More doc tweaking.

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
 #' Gospel of Mark textual variation data extracted from UBS4 (*Greek New Testament*, 4th rev. ed., Stuttgart: United Bible Societies, 1993).
 #'
-#' A data frame that specifies the readings of witnesses for variation sites found in the UBS4 apparatus. Numerals encode readings according to the order given in the apparatus: i.e. 1 = first reading, 2 = second reading, etc. There is one row per witness and one column per variation site contained in the apparatus. This data frame was constructed by Richard Mallett.
+#' A data frame that specifies the readings of witnesses for variation sites found in the UBS4 apparatus (one row per witness and one column per variation site). Numerals encode readings according to the order given in the apparatus: i.e. 1 = first reading, 2 = second reading, etc. This data frame was constructed by Richard Mallett.
 #'
 #' @format A data frame with 267 rows and 142 columns:
 #' \describe{
