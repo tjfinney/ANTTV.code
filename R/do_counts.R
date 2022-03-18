c8a0cdfebd4af348c06b50faada0dfce50160b6d
Tim Finney <tjf@tfinney.net>
2022-03-18 13:51:32 +0800

    Tweak do_count doc again.

--- R/do_counts.R
+++ R/do_counts.R
@@ -1,6 +1,6 @@
 #' Compute pairwise defined counts
 #'
-#' Compute how many columns in a data set are pairwise defined for each combination of two rows. A column is pairwise defined if its elements are defined for both rows. (That is, neither row has `NA`.) Returns a square matrix giving the count for every combination of two rows.
+#' Compute how many columns in a data set are pairwise defined for each combination of two rows. A column is pairwise defined if its elements are defined for both rows (that is, neither row has `NA`). Returns a square matrix giving the count for every combination of two rows.
 #'
 #' @param fr A data frame.
 #'
