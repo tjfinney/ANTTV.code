421b263acbef1f62bdce23f2e4a4a469ecc19bd7
Tim Finney <tjf@tfinney.net>
2022-04-20 07:15:38 +0800

    Edit do_NJ() to use plot, write, and fn args.

--- R/do_NJ.R
+++ R/do_NJ.R
@@ -4,16 +4,25 @@
 #'
 #' @param dm A distance matrix.
 #'
-#' @return A neighbour-joining tree plot.
+#' @param plot logical flag: (default = TRUE) if TRUE then a plot will be drawn on the current graphics device.
+#'
+#' @param write logical flag: (default = FALSE) if TRUE then a plot will be written to file fn.
+#'
+#' @return An object of class "phylo".
 #' @export
 #'
-do_NJ <- function(dm) {
+do_NJ <- function(dm, plot = TRUE, write = FALSE, fn = "../NJ/Mark.UBS4.png") {
   # Perform analysis.
   tr <- ape::nj(stats::as.dist(dm))
   # Do plot
-  grDevices::graphics.off()
-  graphics::par(bg="white")
-  graphics::par(mai=c(0.5, 0.5, 0.5, 0.5))
-  #plot(tr, "u")
+  if (plot) {
+    grDevices::graphics.off()
+    graphics::par(bg="white")
+    graphics::par(mai=c(0.5, 0.5, 0.5, 0.5))
+    plot(tr, "u")
+  }
+  if (write) {
+    grDevices::dev.print(grDevices::png, file=fn, width=600)
+  }
   tr
 }
