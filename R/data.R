29c80af48e91eb39e918493eb7806c60b8e1367e
Tim Finney <tjf@tfinney.net>
2022-05-15 16:09:50 +0800

    Add Acts.UBS2.dist documentation.

--- R/data.R
+++ R/data.R
@@ -9,3 +9,15 @@
 #' }
 #' @source \url{https://zenodo.org/record/6466262}
 "Mark.UBS4"
+
+#' Acts of the Apostles textual variation data extracted from UBS2 (*Greek New Testament*, 2nd ed.).
+#'
+#' A distance matrix specifying simple matching distances between a set of witnesses. The distance matrix was constructed by Maurice Robinson. See Timothy J. Finney, "Varieties of New Testament Text" in *Digging for the Truth: Collected Essays Regarding the Byzantine Text of the Greek New Testament*, ed. Mark Billington and Peter Streitenberger (Norden: FYM, 2014) for details of how the distance matrix was constructed. Witnesses are named according to the Gregory-Aland system.
+#'
+#' @format A distance matrix with 55 rows and 55 columns:
+#' \describe{
+#'   \item{P74}{Witness P74}
+#'   \item{A}{Witness A}
+#' }
+#' @source \url{https://zenodo.org/record/6505843}
+"Acts.UBS2.dist"
--- tests/testthat/test-read_dist_matrix.R
+++ tests/testthat/test-read_dist_matrix.R
@@ -1,7 +1,7 @@
 test_that("read_dist_matrix() reads a distance matrix", {
   dm <- read_dist_matrix("https://zenodo.org/record/6505843/files/Acts.UBS2.dist.csv")
   diag.zero <- vector(mode="numeric", length=dim(dm)[1])
-  expect_equal(dm, Acts.UBS2.dist.15)
+  expect_equal(dm, Acts.UBS2.dist)
   expect_equal(as.vector(diag(dm)), diag.zero)
   expect_equal(rownames(dm), colnames(dm))
 })
