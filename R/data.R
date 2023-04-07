c9333c6425142db1c63a86dad44a990e58cf38f3
Tim Finney <tjf@tfinney.net>
2023-04-07 20:10:59 +0800

    Edit description

--- R/data.R
+++ R/data.R
@@ -3,10 +3,6 @@
 #' A data frame that specifies the readings of witnesses for variation sites found in the UBS4 apparatus (one row per witness and one column per variation site). Numerals encode readings according to the order given in the apparatus: i.e. 1 = first reading, 2 = second reading, etc. This data frame was constructed by Richard Mallett.
 #'
 #' @format A data frame with 267 rows and 142 columns:
-#' \describe{
-#'   \item{Mk.1.1}{Codes representing readings found in the apparatus entry at Mark 1.1.}
-#'   ...
-#' }
 #' @source \url{https://zenodo.org/record/6466262}
 "Mark.UBS4"
 
