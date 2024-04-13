c8c0d1a9318ae6b5e4adef4d90cfa1233bf9156f
Tim Finney <tjf@tfinney.net>
2024-04-13 12:06:18 +0800

    Write checks for do_trim()

---
+++ R/do_trim.R
@@ -0,0 +1,25 @@
+#' Drop witnesses from the input distance matrix if they are within a specified distance of a reference witness.
+#'
+#' @param dm A distance matrix.
+#'
+#' @param ref the reference witness (e.g. "Byz").
+#'
+#' @param d the specified distance (e.g. 0.25).
+#'
+#' @return A matrix
+#' @export
+#'
+do_trim <- function(dm, ref="Byz", d=0.25) {
+  tryCatch(
+    expr = {
+      # Do checks
+      stopifnot(is.matrix(dm))
+      stopifnot(ref %in% rownames(dm))
+      stopifnot(d > 0)
+      stopifnot(d < 1)
+      # Find witnesses within d of ref
+      # Return result
+      dm
+    }
+  )
+}
